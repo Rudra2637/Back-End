6f3901d8edafa2ca9a8ff97db1a44f4dcea6c40d
Rudra2637 <singhrudra2637@gmail.com>
2025-07-03 16:57:53 +0530

    All Controllers Written

--- Project/src/controllers/tweet.controller.js
+++ Project/src/controllers/tweet.controller.js
@@ -10,22 +10,24 @@ const createTweet = asyncHandler(async (req, res) => {
     //Get the tweet from the form
     //Check the necessary validations 
     //Put the tweet in the database 
-    // const {content} = req.body
-    // // console.log(req.body)
-    // // console.log("tweet ",content)
-    // if(content.trim() === "")throw new ApiError(400,"Fill the required fields");
+    const {content} = req.body
+    // console.log(req.body)
+    // console.log("tweet ",content)
+    if(content.trim() === "")throw new ApiError(400,"Fill the required fields");
     
     
-    // const owner = await User.findById(req.user._id)
-    // if(!owner)throw new ApiError(401,"Invalid User");
-    // // console.log("owner ",owner)
-    // const create = await Tweet.create({
-    //     content,
-    //     owner
-    // }) 
+    const owner = await User.findById(req.user._id).select(
+        "-password -refreshToken"
+    )
+    if(!owner)throw new ApiError(401,"Invalid User");
+    // console.log("owner ",owner)
+    const create = await Tweet.create({
+        content,
+        owner
+    }) 
 
-    // return res.status(200)
-    // .json(new ApiResponse(200,create,"Tweet created successfully"))
+    return res.status(200)
+    .json(new ApiResponse(200,create,"Tweet created successfully"))
 
 })
 
@@ -33,46 +35,101 @@ const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
     //Get the userId from the url
     //Retrieve the user tweets by making call in the database 
-    // const {userId} = req.params
-    // if(!userId)throw new ApiError(400,"Invalid User id");
+    const {userId} = req.params
+    if(!userId)throw new ApiError(400,"Invalid User id");
 
-    // const user = await User.findById(req.user._id)
+    const user = await User.findById(req.user._id)     //Can do this and also there is one more way to do this
+
+    const content = await Tweet.aggregate([
+        {
+            $lookup:{
+                from:"users",
+                localField:"owner",
+                foreignField:"_id",
+                as:"owner"
+            }
+        },
+        {$unwind:"$owner"},
+        {
+            $project:{
+                userName:1,
+                avatar:1,
+                coverImage:1,
+                content:1,
+                createdAt:1,
+                updatedAt:1,
+                userName:"$owner.userName",
+                avatar:"$owner.avatar",
+                coverImage:"$owner.coverImage",
+            }
+        }
+    ])
 
     // const content = await Tweet.aggregate([
     //     {
-    //         $lookup:{
-    //             from:"User",
-    //             localField:"owner",
-    //             foreignField:"_id",
-    //             as:"owner"
+    //         $match: {
+    //             owner: new mongoose.Types.ObjectId(userId)
     //         }
     //     },
-        
     //     {
-    //         $project:{
-    //             userName:1,
-    //             avatar:1,
-    //             coverImage:1,
-    //             content:1,
-    //             createdAt:1,
-    //             updatedAt:1,
-                
+    //         $lookup: {
+    //             from: "users",
+    //             localField: "owner",
+    //             foreignField: "_id",
+    //             as: "owner"
+    //         }
+    //     },
+    //     { $unwind: "$owner" },
+    //     {
+    //         $project: {
+    //             content: 1,
+    //             createdAt: 1,
+    //             updatedAt: 1,
+    //             userName: "$owner.userName",
+    //             avatar: "$owner.avatar",
+    //             coverImage: "$owner.coverImage"
     //         }
     //     }
-    // ])
-    // if(content.length === 0)throw new ApiError(401,"No tweet made by user");
-    // console.log("content ",content)
-    // return res.status(200)
-    // .json(new ApiResponse(200,content[0],"Tweets fetched successfully"))
+    // ]);
+
+
+
+    if(content.length === 0)throw new ApiError(401,"No tweet made by user");
+    console.log("content ",content)
+    return res.status(200)
+    .json(new ApiResponse(200,content[0],"Tweets fetched successfully"))
 
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId} = req.params
+    const {content} = req.body
+    
+    const tweet = await Tweet.findByIdAndUpdate(tweetId,
+        {
+            $set:{
+                content
+            }
+        },
+        {new:true}
+    )
+    if(!tweet)throw new ApiError(401,"Invalid Req");
+
+    return res.status(200)
+    .json(new ApiResponse(200,tweet,"Tweet updated Successfully"))
+    
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const{tweetId} = req.params
+
+    const tweet = await Tweet.findByIdAndDelete(tweetId)
+    if(!tweet)throw new ApiError(401,"Invalid Req");
+
+    return res.status(200)
+    .json(new ApiResponse(200,tweet,"Tweet Deleted successfully"))
 })
 
 export {
--- Project/src/controllers/video.controller.js
+++ Project/src/controllers/video.controller.js
@@ -15,26 +15,101 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    if(title.trim() === "" || description.trim() === "")throw new ApiError(402,"All fields required");
+    
+    const thumbnailPath = req.files?.thumbnail?.[0]?.path
+
+    const videoFilePath = req.files?.videoFile?.[0]?.path
+
+    const uploadThumbnail = await uploadOnCloudinary(thumbnailPath)
+    const uploadVideo = await uploadOnCloudinary(videoFilePath)
+
+    const duration  = uploadVideo?.duration || 0
+    const videoFile = uploadVideo?.secure_url
+    const thumbnail = uploadThumbnail?.secure_url
+
+    const owner = req.user?._id
+
+    const video = await Video.create({
+        title,
+        description,
+        videoFile,
+        thumbnail,
+        owner,
+        duration,
+    })
+
+    if(!video)throw new ApiError(402,"Error in publishing video");
+
+    return res.status(201).json(
+        new ApiResponse(201, video, "Video published successfully")
+    )
+
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    const video = await Video.findById(videoId)
+
+    if(!video)throw new ApiError(402,"Invalid videoId");
+
+    return res.status(200).json
+    (new ApiResponse(200,video,"Video fetched"))
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    const  {title,description} = req.body
+    const thumbnailPath = req.files?.thumbnail[0].path
+
+    const thumbnail = await uploadOnCloudinary(thumbnailPath)
+
+    if(!thumbnail)throw new ApiError(404,"Error in uploading thumbnail");
+
+    const video = await Video.findByIdAndUpdate(videoId,
+        {
+            $set:{
+                title,
+                description,
+                thumbnail:thumbnail.url
+            }
+        },
+        {new:true}
+    )
+
+    if(!video)throw new ApiError(401,"Something went wrong");
+
+    return res.status(200)
+    .json(
+        new ApiResponse(200,video,"Video updated Successfully")
+    )
 
 })
 
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    const video = await Video.findByIdAndDelete(videoId)
+    return res.status(200)
+    .json(
+        new ApiResponse(200,video,"Video Removed successfully")
+    )
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
+    const video = await Video.findById(videoId)
+     if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+    video.isPublished = !video.isPublished
+    await video.save()
+    
+    return res.status(200).json(
+        new ApiResponse(200, video, "Video publish status toggled successfully")
+    );
 })
 
 export {
